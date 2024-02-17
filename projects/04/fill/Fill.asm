750a309d121757d1eb58b93e148d7b44cb3a0274
rysky-boi <dave@rysk.finance>
2024-02-17 00:29:06 +0000

    feat: fill task

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,57 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 //// Replace this comment with your code.
+
+// Set complete to 8192 after the start of screen
+@SCREEN
+    D=A
+
+@8192 // This comes from the 32 words by 256 rows
+    D=D+A
+
+@COMPLETE
+    M = D
+
+// comeback to the start and go again
+(START) // if the keyboard is pressed val is = -1 else it is 0
+    @KBD
+    D=M
+    @BLACK
+    D; JNE
+
+    @val
+    M=0
+
+(CONTINUE)
+    // set address to the start of the screen memory map
+    @SCREEN
+    D=A
+    @address
+    M=D
+
+(LOOP) // check if the address is out of bounds 
+    @COMPLETE
+    D=M
+    @address
+    D=M-D
+    @START
+    D; JGE
+
+    @val
+    D=M
+    @address
+    A=M
+    M=D
+
+    @address
+    M=M+1
+
+    @LOOP
+    0; JMP
+
+(BLACK)
+    @val
+    M=-1
+
+@CONTINUE
+    0; JMP
